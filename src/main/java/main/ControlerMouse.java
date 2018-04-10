635ff94a57451927a4830cb89a4d9642c831628f
julien.gonzalez <julien.gonzalez72@gmail.com>
2018-04-10 15:44:41 +0200

    controleur

--- src/main/java/main/ControlerMouse.java
+++ src/main/java/main/ControlerMouse.java
@@ -3,42 +3,19 @@ package main;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import javax.swing.JTextPane;
-import javax.swing.text.BadLocationException;
-
 public class ControlerMouse implements MouseListener{
 	
-	Panneau view;
+	private Panneau view;
+	private TextHandler handler;
 	
-	public ControlerMouse(Panneau p){
+	public ControlerMouse(Panneau p, TextHandler handler){
 		view = p;
+		this.handler = handler;
 	}
 
 	public void mouseClicked(MouseEvent e) {
-		JTextPane panel = view.editorPane;
-		int positionClic = panel.getCaretPosition();
-		int longueur = 1;
-		String word = " ";
-		try {
-			if ( Character.isAlphabetic(panel.getText(positionClic+1, 1).toCharArray()[0])){
-				word += panel.getText(positionClic+1, 1);
-			}
-		} catch (BadLocationException e2) {
-			
-		}
-		while( Character.isAlphabetic(word.toCharArray()[word.length()-1])){
-			longueur++;
-			try {
-				word += panel.getText(positionClic+longueur, 1);
-			} catch (BadLocationException e1) {
-				break;
-			}
-		}
-		try {
-			System.out.println(panel.getText(positionClic, longueur));
-		} catch (BadLocationException e1) {
-			
-		}
+		if (handler.correctPause(view.editorPane.getCaretPosition()))
+			view.editorPane.insert(view.editorPane.getCaretPosition(), "/");
 	}
 
 	public void mouseEntered(MouseEvent e) {

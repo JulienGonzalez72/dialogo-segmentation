fe2b353af9d77b51e35d0c3cd2e811d5ac3e500b
julien.gonzalez <julien.gonzalez72@gmail.com>
2018-04-10 17:36:59 +0200

    last commit

--- src/main/java/main/Panneau.java
+++ src/main/java/main/Panneau.java
@@ -8,16 +8,8 @@ public class Panneau extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
-<<<<<<< HEAD
 	//panneau du texte
 	public TextPane editorPane;
-=======
-	//panneau du texte sans cesure
-	public TextPane editorPane;
-
-	//contient le texte avec cesure
-	public String texteAvecCesure;
->>>>>>> a759dec80509cf7b562d839c99832cfd29bb5bdc
 
 	public Panneau() throws IOException {
 
@@ -29,17 +21,11 @@ public class Panneau extends JPanel {
 		this.setLayout(new GridLayout(1, 1));
 		
 		editorPane = new TextPane();
-<<<<<<< HEAD
 		
 		editorPane.setText(texte);
-=======
-		editorPane.setText(getTextFromFile("ressources/textes/Ah les crocodiles"));
->>>>>>> a759dec80509cf7b562d839c99832cfd29bb5bdc
 		editorPane.setEditable(false);	
 		editorPane.addMouseListener(controlerMouse);
 		this.add(editorPane);
-
-		texteAvecCesure = getTextFromFile("ressources/textes/Ah les crocodiles C");
 	}
 	
 	/**
@@ -54,7 +40,7 @@ public class Panneau extends JPanel {
 		String toReturn = "";
 		String ligneCourante = br.readLine();
 		while ( ligneCourante != null){
-			toReturn += ligneCourante;
+			toReturn += ligneCourante + " ";
 			ligneCourante = br.readLine();
 		}
 		br.close();

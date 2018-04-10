d0b88c89a80e1c7bae705d2d186a9ac72a434685
julien.gonzalez <julien.gonzalez72@gmail.com>
2018-04-10 15:44:20 +0200

    gestion de texte

---
+++ src/main/java/main/TextHandler.java
@@ -0,0 +1,38 @@
+package main;
+
+public class TextHandler {
+	
+	private String ctxt;
+	private String txt;
+	
+	public TextHandler(String texteOriginal, String texteAvecCesures) {
+		this.txt = texteOriginal;
+		this.ctxt = texteAvecCesures;
+	}
+	
+	/**
+	 * Indique si la césure est placée au bon endroit.
+	 */
+	public boolean correctPause(int offset) {
+		StringBuilder b = new StringBuilder(ctxt);
+		for (int i = 0; i < b.length(); i++) {
+			if (i >= offset) {
+				break;
+			}
+			/// supprime le slash et l'espace avant ///
+			if (b.charAt(i) == '/') {
+				b.deleteCharAt(i);
+				b.deleteCharAt(i - 1);
+			}
+		}
+		return b.charAt(++offset) == '/';
+	}
+	
+	/**
+	 * L'utilisateur marque la pause à cet endroit là
+	 */
+	public void markPause(int offset) {
+		
+	}
+	
+}

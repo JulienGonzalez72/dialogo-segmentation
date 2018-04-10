1ad2696d3cf0220b66525558afd82e9472b0f665
julien.gonzalez <julien.gonzalez72@gmail.com>
2018-04-10 15:48:38 +0200

    last commit

--- src/main/java/main/ControlerMouse.java
+++ src/main/java/main/ControlerMouse.java
@@ -1,45 +1,21 @@
 package main;
 
-import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import javax.swing.text.*;
 
-<<<<<<< HEAD
-public class ControlerMouse implements MouseListener{
-	
-	private Panneau view;
-	private TextHandler handler;
-	
-	public ControlerMouse(Panneau p, TextHandler handler){
-=======
 public class ControlerMouse implements MouseListener {
 
 	Panneau view;
+	TextHandler handler;
 
-	public ControlerMouse(Panneau p) {
->>>>>>> a759dec80509cf7b562d839c99832cfd29bb5bdc
+	public ControlerMouse(Panneau p, TextHandler handler) {
 		view = p;
 		this.handler = handler;
 	}
 
 	public void mouseClicked(MouseEvent e) {
-<<<<<<< HEAD
 		if (handler.correctPause(view.editorPane.getCaretPosition()))
 			view.editorPane.insert(view.editorPane.getCaretPosition(), "/");
-=======
-		String texteAvecCesure = view.texteAvecCesure;
-		TextPane panel = view.editorPane;
-		int positionClic = panel.getCaretPosition();
-		TextPane panelCesure = new TextPane();
-		panelCesure.setText(texteAvecCesure);
-		try {
-			System.out.println(panel.getText(positionClic, 1));
-		} catch (BadLocationException e1) {
-			e1.printStackTrace();
-		}
-		view.editorPane.surlignerPhrase(positionClic, Color.green);
->>>>>>> a759dec80509cf7b562d839c99832cfd29bb5bdc
 	}
 
 	public void mouseEntered(MouseEvent e) {

7201da86bb736f18c7b2a15e4a1ac718efaa01d7
roro <romandiezpro@gmail.com>
2018-04-10 11:19:22 +0200

    Creation de la fenere et du panneau principal

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Roman et Julien etudient GIT");
+		new Fenetre("Roman et Julien", 500, 500);
 	}
 	
 }

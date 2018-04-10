cf824bdcc1719ea5f9643de746acad69a038a887
roro <romandiezpro@gmail.com>
2018-04-10 11:40:12 +0200

    Test lecture fichier texte

---
+++ src/main/java/main/Panneau.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.awt.*;
+import java.io.*;
+import java.util.stream.Stream;
+
+import javax.swing.*;
+
+public class Panneau extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public Panneau() throws IOException {
+		this.setLayout(new GridLayout(1, 1));
+		JEditorPane editorPane = new JEditorPane();
+		this.add(editorPane);
+
+		File fichierTxt = new File("ressources/textes/dameDeFoix.txt");
+		InputStream ips = null;
+		ips = new FileInputStream(fichierTxt);
+		InputStreamReader ipsr = new InputStreamReader(ips);
+		BufferedReader br = new BufferedReader(ipsr);
+		String chaine = "";
+		Stream<String> lignes = br.lines();
+		br.close();
+	}
+
+}

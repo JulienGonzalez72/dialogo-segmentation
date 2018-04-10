5e4146bab3be0fbe346be9e632bae008a3387477
julien.gonzalez <julien.gonzalez72@gmail.com>
2018-04-10 13:49:15 +0200

    Modifications essentielles

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,15 +3,7 @@ package main;
 public class Main {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-		System.out.println("CHANGEMENT2");
-=======
-		System.out.println("Roman et Julien etudient GIT");
->>>>>>> cd4f33623c450b84a2e0a5d4d2e96115b05c5a47
-=======
 		new Fenetre("Roman et Julien", 500, 500);
->>>>>>> 7201da86bb736f18c7b2a15e4a1ac718efaa01d7
 	}
 	
 }

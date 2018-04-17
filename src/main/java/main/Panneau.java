bc14075ea53d0e0552d1ffb98aa9232c1a57ccfd
roro <romandiezpro@gmail.com>
2018-04-17 11:13:05 +0200

    amélioration build pages

--- src/main/java/main/Panneau.java
+++ src/main/java/main/Panneau.java
@@ -58,7 +58,7 @@ public class Panneau extends JPanel {
 		this.add(editorPane, BorderLayout.CENTER);
 
 		/// construit la mise en page virtuelle ///
-		buildPages(FenetreParametre.premierSegment - 1);
+		buildPagesByRoman(FenetreParametre.premierSegment - 1);
 		/// affiche la première page ///
 		afficherPageSuivante();
 		/// calcule le nombre de pages total ///
@@ -66,33 +66,33 @@ public class Panneau extends JPanel {
 
 		/// initialise le lecteur et le démarre ///
 		player = new Player(textHandler);
-		player.onBlockEnd.add(new Runnable() {		
+		player.onBlockEnd.add(new Runnable() {
 			@Override
 			public void run() {
-				setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));		
+				setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 			}
 		});
-		player.onPhraseEnd.add(new Runnable() {		
+		player.onPhraseEnd.add(new Runnable() {
 			@Override
 			public void run() {
 				Toolkit tk = Toolkit.getDefaultToolkit();
 				Image img = tk.getImage("parler.png");
 				Cursor monCurseur = tk.createCustomCursor(img, new Point(16, 16), "parler.png");
-				setCursor(monCurseur);		
+				setCursor(monCurseur);
 			}
 		});
-		player.onPlay.add(new Runnable() {		
+		player.onPlay.add(new Runnable() {
 			@Override
 			public void run() {
 				Toolkit tk = Toolkit.getDefaultToolkit();
 				Image img = tk.getImage("ecouter.png");
 				Cursor monCurseur = tk.createCustomCursor(img, new Point(16, 16), "ecouter.png");
-				setCursor(monCurseur);	
+				setCursor(monCurseur);
 			}
 		});
 		player.goTo(FenetreParametre.premierSegment - 1);
 		controlFrame = new ControlFrame(player);
-		
+
 		ControlerKey controlerKey = new ControlerKey(player);
 		editorPane.addKeyListener(controlerKey);
 		editorPane.requestFocus();
@@ -162,6 +162,86 @@ public class Panneau extends JPanel {
 		}
 	}
 
+	public void buildPagesByRoman(int startPhrase) {
+		segmentsEnFonctionDeLaPage.clear();
+		float m = Constants.TEXTPANE_MARGING;
+		//numero de la page courante
+		int page = 1;
+		//numero du segment courant
+		int segment = 0;
+		//numero de la lettre courante du segment courant
+		int lettre = 0;
+		List<Integer> segments = new ArrayList<>();
+		String chaineSegmentCourant = "";
+		String chaineLigneCourante = "";
+		boolean finis = false;
+		//tant qu'on a pas finis
+		while (!finis) {
+			//on fait ce for tant que il reste de la place pour une ligne de plus dans la page
+			for (int i = 1; !finis && (getHauteur("|") *3* (i)) < (getHeight() - m); i++) {
+				//on fait ce for tant qu'il reste de la palce pour une lettre dans la ligne
+				while (!finis && getLargeur(chaineLigneCourante) < (getWidth() - (m * 2))) {
+					//si on a finis un segment
+					if (!finis && chaineSegmentCourant.length() == textHandler.getPhrase(segment).length()) {
+						//on reinitialise la chaine qui contient le segment en construction
+						chaineSegmentCourant = "";
+						//on ajoute le segment à la liste des segments de la page
+						segments.add(segment);
+						//si on a placé tous les segments
+						if (segment + 1 == textHandler.getPhrasesCount()) {
+							//on a finis l'algo
+							finis = true;
+						}
+						//on passe au segment suivant
+						segment++;
+						//on revient a la lettre 0
+						lettre = 0;
+					}
+					//si on a pas finis l'algo
+					if (!finis) {
+						//on rajoute a la chaine du segment courant et a lla chaine de la lignecourante la lettre courante du segment courant
+						chaineSegmentCourant += textHandler.getPhrase(segment).charAt(lettre);
+						chaineLigneCourante += textHandler.getPhrase(segment).charAt(lettre);
+						//on passe à la ligne suivante
+						lettre++;
+					}
+				}
+				//on reinitialise la chaine de la ligne courante
+				chaineLigneCourante = "";
+				/*System.out.println();
+				System.out.println("Page numero : "+page);
+				System.out.println("Ligne numero : "+(i));
+				System.out.println("Taille totale disponible : "+(getHeight() - m));
+				System.out.println("Taille après ajout d'une nouvelle ligne : "+(getHauteur("|") *3* (i+1)));
+				System.out.println();*/
+			}
+			//on ajoute les segments de la page dans la page
+			segmentsEnFonctionDeLaPage.put(page, segments);
+			//la liste des segments courant est reinitialisée
+			segments = new ArrayList<>();
+			//on passe à la page suivante
+			page++;
+		}
+	}
+
+	private double getLargeur(String s) {
+		return editorPane.getTextBounds(s).getWidth();
+	}
+
+	private double getHauteur(String s) {
+		return editorPane.getTextBounds(s).getHeight();
+	}
+	
+	private double getHauteurMax(String s) {
+		double r = 0.0;
+		for (char c : s.toCharArray()) {
+			if ( getHauteur(String.valueOf(c)) > r) {
+				r = getHauteur(String.valueOf(c));
+			} 
+		}
+		return r;
+	}
+
 	public void showPage(int page) {
 		fenetre.setTitle("Lexidia - Page " + page);
 		String texteAfficher = "";
@@ -223,18 +303,20 @@ public class Panneau extends JPanel {
 	 */
 	public void surlignerSegment(Color c, int n) {
 		if (textHandler.getPhrase(n) != null) {
-			int debutRelatifSegment = textHandler.getRelativeStartPhrasePosition(FenetreParametre.premierSegment - 1,n);
+			int debutRelatifSegment = textHandler.getRelativeStartPhrasePosition(FenetreParametre.premierSegment - 1,
+					n);
 			int finRelativeSegment = debutRelatifSegment + textHandler.getPhrase(n).length();
 			editorPane.surlignerPhrase(debutRelatifSegment, finRelativeSegment, Constants.RIGHT_COLOR);
 		}
 	}
-	
+
 	/**
 	 * Colorie tout jusqu'au segment n en couleur c
 	 */
 	public void surlignerJusquaSegment(Color c, int n) {
 		if (textHandler.getPhrase(n) != null) {
-			int debutRelatifSegment = textHandler.getRelativeStartPhrasePosition(FenetreParametre.premierSegment - 1,n);
+			int debutRelatifSegment = textHandler.getRelativeStartPhrasePosition(FenetreParametre.premierSegment - 1,
+					n);
 			int finRelativeSegment = debutRelatifSegment + textHandler.getPhrase(n).length();
 			editorPane.surlignerPhrase(0, finRelativeSegment, Constants.RIGHT_COLOR);
 		}

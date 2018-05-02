c62975f0fb0bd29db88e686d39c8e490d7e0c3b5
roro <romandiezpro@gmail.com>
2018-05-02 14:21:18 +0200

    look andfeel de BG

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,24 +1,19 @@
 package main;
 
-import java.awt.Font;
-import java.awt.FontFormatException;
-import java.awt.GraphicsEnvironment;
-import java.awt.Point;
-import java.io.File;
-import java.io.IOException;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
 
+import com.incors.plaf.kunststoff.KunststoffLookAndFeel;
 import main.view.FenetreParametre;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e1) {
+			UIManager.setLookAndFeel(new KunststoffLookAndFeel());
+		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
 		
@@ -27,16 +22,11 @@ public class Main {
 			try {
 				GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 				ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("ressources/fonts/" + s)));
-
 			} catch (IOException | FontFormatException e) {
-				e.printStackTrace();
+				//e.printStackTrace();
 			}
 		}
-		try {
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				Point p = new Point(50, 0);

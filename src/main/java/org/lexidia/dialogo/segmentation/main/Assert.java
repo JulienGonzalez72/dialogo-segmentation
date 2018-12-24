4824709b592f3105e41d9db5194f0c91fe608477
Julien Gonzalez <julien.gonzalez72@gmail.com>
2018-12-24 09:26:49 +0100

    classe Assert

---
+++ src/main/java/org/lexidia/dialogo/segmentation/main/Assert.java
@@ -0,0 +1,67 @@
+package org.lexidia.dialogo.segmentation.main;
+
+import java.awt.Font;
+import java.util.Arrays;
+
+import org.lexidia.dialogo.segmentation.controller.Pilot;
+
+/**
+ * Classe utilitaire d'assertion qui lance des exceptions, elle sert notamment au ControllerText.
+ * @author Julien Gonzalez
+ */
+public final class Assert {
+	
+	public static void assertFont(Font font) throws IllegalArgumentException {
+		assertNotNull(font, "font");
+		assertPositive(font.getSize(), "Font size");
+		assertNotEmpty(font.getFamily(), "Font family");
+	}
+	
+	public static void assertNotNull(Object obj, String varName) throws IllegalArgumentException {
+		if (obj == null) {
+			error(varName + " is null !");
+		}
+	}
+	
+	public static void assertNotEmpty(String str, String varName) throws IllegalArgumentException {
+		assertNotNull(str, varName);
+		if (str.isEmpty()) {
+			error(varName + " must not be empty !");
+		}
+	}
+	
+	public static void assertPositiveOrNull(double d, String varName) throws IllegalArgumentException {
+		if (d < 0) {
+			error(varName + " (" + d + ") must be positive or null !");
+		}
+	}
+	
+	public static void assertPositive(double d, String varName) throws IllegalArgumentException {
+		if (d <= 0) {
+			error(varName + " (" + d + ") must be strictly positive !");
+		}
+	}
+	
+	public static <T> void assertContains(T obj, String varName, T... possibleValues) throws IllegalArgumentException {
+		if (Arrays.binarySearch(possibleValues, obj, null) < 0) {
+			error("Unacceptable value for " + varName + " (possible values are " + Arrays.toString(possibleValues) + ")");
+		}
+	}
+	
+	public static void assertGreater(double d1, double d2, String varName) throws IllegalArgumentException {
+		if (d1 <= d2) {
+			error(varName + " (" + d1 + ") must be greater than " + d2 + " !");
+		}
+	}
+	
+	public static void assertNotStarted(Pilot pilot, String action) throws IllegalStateException {
+		if (pilot.hasStarted()) {
+			throw new IllegalStateException("Can't " + action + " while the exercise has started !");
+		}
+	}
+	
+	private static void error(String message) throws IllegalArgumentException {
+		throw new IllegalArgumentException(message);
+	}
+	
+}
